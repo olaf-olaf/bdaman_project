7fb62ac841f2e6dd0df124c042a397359f6b1ee4
Olaf <olaf-olaf@live.nl>
2018-11-30 19:44:40 +0100

    WIP of menu. Added scrolling functionality for player one.

--- Assets/Scripts/partSelector.cs
+++ Assets/Scripts/partSelector.cs
@@ -5,45 +5,87 @@ using UnityEngine.UI;
 
 public class partSelector : MonoBehaviour {
     public GameObject[] sqauds;
+    public GameObject[] feetParts;
+    public GameObject[] cannonparts;
     private int bodyPart;
+    private int feetIndex;
+    private int cannonIndex;
 
     // Use this for initialization
     void Start () {
         bodyPart = 0;
+        feetIndex = 0;
+        cannonIndex = 0;
         sqauds[bodyPart].GetComponent<Renderer>().material.color = Color.green;
+        feetParts[feetIndex].SetActive(true);
     }
 	
 	// Update is called once per frame
 	void Update () {
+
+        // Scroll through different sections of the body
         if (Input.GetKeyDown(KeyCode.DownArrow)) {
-            print("downarrow");
-            bodyPart = (bodyPart + 1) % sqauds.Length;
-            setPartSelectColor();
-            print(bodyPart);
+            selectBodySection();
          }
-        // EDIT THIS ONCE IF FIGURED OUT HOW TO GO FROM 0 to 2 with uparrow.
-        //else if (Input.GetKeyDown(KeyCode.UpArrow))
-        //{
-        //    print("UPARROW");
-        //    bodyPart = (bodyPart - 1) % sqauds.Length;
-        //    print(bodyPart);
-        //}
+
+        // Select feet
+        if(bodyPart == 2 && Input.GetKeyDown("right")){
+            selectFeetPart();
+
+        }
+
+        // Select cannon
+        if (bodyPart == 1 && Input.GetKeyDown("right"))
+        {
+            selectCannonPart();
+        }
+    }
+
+    void selectCannonPart(){
+
+        cannonIndex = (cannonIndex + 1) % cannonparts.Length;
+        for (int i = 0; i < cannonparts.Length; i++)
+        {
+            if (i == cannonIndex)
+            {
+                cannonparts[i].SetActive(true);
+            }
+            else
+            {
+                cannonparts[i].SetActive(false);
+            }
+        }
+
+    }
+
+
+    void selectFeetPart(){
+        feetIndex = (feetIndex + 1) % feetParts.Length;
+        for (int i = 0; i < feetParts.Length; i++)
+        {
+            if (i == feetIndex)
+            {
+                feetParts[i].SetActive(true);
+            } else {
+                feetParts[i].SetActive(false);
+            }
+        }
+    }
+
+    void selectBodySection(){
+        bodyPart = (bodyPart + 1) % sqauds.Length;
+        setPartSelectColor();
+
     }
 
     void setPartSelectColor(){
         for (int i = 0; i < sqauds.Length; i++){
             if (i == bodyPart){
                 sqauds[i].GetComponent<Renderer>().material.color = Color.green;
-                //rend.material.color = Color.green;
-                //print("TEE");
-
             } else {
                 sqauds[i].GetComponent<Renderer>().material.color = Color.white;
 
-                //print("HEE");
-
             }
-
         }
 
 
