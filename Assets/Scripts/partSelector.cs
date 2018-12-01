30875711e961505199db713b038ab2635d3c30f9
Olaf <olaf-olaf@live.nl>
2018-12-01 16:33:53 +0100

    Added all stats. WIP to properly display them.

--- Assets/Scripts/partSelector.cs
+++ Assets/Scripts/partSelector.cs
@@ -8,6 +8,10 @@ public class partSelector : MonoBehaviour {
     public GameObject[] feetParts;
     public GameObject[] cannonParts;
     public GameObject[] armParts;
+    public GameObject currentSpeedDisplay;
+    private Dictionary<string, float[]> partstats = new Dictionary<string, float[]>();
+    private float[] current_stats = { 120, 1, 0.3f, 20, 3, 5 };
+
 
     private int armIndex;
     private int bodyPart;
@@ -15,19 +19,40 @@ public class partSelector : MonoBehaviour {
     private int cannonIndex;
 
     // Use this for initialization
+    // these values need to be set in the game menu
+    /*         NAME           Default               Range
+     * movement_speed           120                  100, 150 
+     * accuracy                  1                   1, 50  
+     * fire_rate                 0.3                 0.1, 0.8
+     * power                     20                  18, 25
+     * reload time               3                   2, 4
+     * mag size                  5                   3, 12 
+    */
     void Start () {
         armIndex = 0;
         bodyPart = 0;
         feetIndex = 0;
         cannonIndex = 0;
+
         sqauds[bodyPart].GetComponent<Renderer>().material.color = Color.green;
         feetParts[feetIndex].SetActive(true);
         cannonParts[cannonIndex].SetActive(true);
         armParts[armIndex].SetActive(true);
+        partstats.Add("feetspeed", new float[]      {20, 0, 0, 0, 0, 0 });
+        partstats.Add("feetbalance", new float[]    {0, 0, 0.2f, 0, 0, 0 });
+        partstats.Add("feetaccuracy", new float[]   {0, 10, 0, 0, 0, 0 });
+        partstats.Add("cannonbalanced", new float[] {0, 0, 0.2f, 0, 0, 0 });
+        partstats.Add("cannonheavy", new float[]    {0, 0, 0, 2, 0, 0 });
+        partstats.Add("armsbalanced", new float[]   {0, 0, 0.1f, 0, 0, 0 });
+        partstats.Add("armscapacity", new float[]   {0, 0, 0, 0, 0, 5 });
+        partstats.Add("armsspeed", new float[]      {10, 0, 0, 2, 0, 0 });
+
+
+
     }
-	
-	// Update is called once per frame
-	void Update () {
+
+    // Update is called once per frame
+    void Update () {
 
         // Scroll through different sections of the body
         if (Input.GetKeyDown(KeyCode.DownArrow)) {
@@ -37,20 +62,75 @@ public class partSelector : MonoBehaviour {
         // Select feet
         if(bodyPart == 2 && Input.GetKeyDown("right")){
             selectPart(ref feetParts, ref feetIndex);
+            getStatsPlayerOne();
+            displayCurrentStats();
 
         }
 
         // Select cannon
         if (bodyPart == 1 && Input.GetKeyDown("right"))
-        {
+        {   
             selectPart(ref cannonParts, ref cannonIndex);
+            getStatsPlayerOne();
+            displayCurrentStats();
         }
 
         // Select arms
         if (bodyPart == 0 && Input.GetKeyDown("right"))
         {
             selectPart(ref armParts, ref armIndex);
+            getStatsPlayerOne();
+            displayCurrentStats();
         }
+
+        // Display stats
+    }
+
+
+    void displayCurrentStats(){
+        int maxSpeed = 150;
+        float currentSpeedRatio = current_stats[0] / maxSpeed;
+        currentSpeedDisplay.transform.localScale = new Vector3(currentSpeedRatio, 1,1);
+    }
+
+    //TODO: Create a helper function that takes a reference to get rid of three
+    //times the same code.
+    void getStatsPlayerOne()
+    {
+        for (int i = 0; i < feetParts.Length; i++)
+        {
+            if (feetParts[i].activeSelf)
+            {
+                for (int j = 0; j < current_stats.Length; j++)
+                {
+                    current_stats[j] = current_stats[j] + partstats[feetParts[i].tag][j];
+                }
+            }
+        }
+        for (int i = 0; i < cannonParts.Length; i++){
+            if (cannonParts[i].activeSelf)
+            {
+                for (int j = 0; j < current_stats.Length; j++)
+                {
+                    current_stats[j] = current_stats[j] + partstats[cannonParts[i].tag][j];
+                }
+            }
+        }
+        for (int i = 0; i < armParts.Length; i++)
+        {
+            if (armParts[i].activeSelf)
+            {
+                for (int j = 0; j < current_stats.Length; j++)
+                {
+                    current_stats[j] = current_stats[j] + partstats[armParts[i].tag][j];
+                }
+            }
+        }
+        // Check if it works.
+        for (int j = 0; j < current_stats.Length; j++){
+            print(current_stats[j]);
+        }
+
     }
 
     void selectPart(ref GameObject[] parts, ref int index){
