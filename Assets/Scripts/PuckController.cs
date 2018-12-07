4d906fc04086e19d48f9cbbb0875f87f0a5ab19e
10800441 <martyn_heijstek@hotmail.com>
2018-12-07 10:09:38 +0100

    Added PUCK game mode

---
+++ Assets/Scripts/PuckController.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PuckController : MonoBehaviour {
+    public Vector3 initialPos; 
+
+    private void Start()
+    { 
+    }
+
+     
+    private IEnumerator RestartBall()
+    {
+
+
+        yield return new WaitForSeconds(2);
+        Debug.Log("in restart bal function");
+
+        transform.position = initialPos; 
+    }
+
+    public void Reset()
+    {
+        StartCoroutine(RestartBall());
+         
+    }
+
+}
