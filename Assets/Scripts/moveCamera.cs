afe01ecae27b849c84ef704c97c479962822eec4
Olaf <olaf-olaf@live.nl>
2018-12-12 12:30:13 +0100

    Finished rotation

--- Assets/Scripts/moveCamera.cs
+++ Assets/Scripts/moveCamera.cs
@@ -9,13 +9,8 @@ public class moveCamera : MonoBehaviour
     public Transform frontPanel;
     private Quaternion backPanelRotation = new Quaternion(0f, 0f, 0f, 0f);
     private Quaternion frontPanelRotation = new Quaternion(0.99f, 0.99f, 0.99f, 0.99f);
-
-
-
-
     public float rotationSpeed = 5;
     private bool gameModeSelection = false;
-    private bool rotating = false;
     // Use this for initialization
     void Start()
     {
@@ -25,23 +20,20 @@ public class moveCamera : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown("enter") && gameModeSelection == false && frontPanelRotation.w < 1f)
+        if (Input.GetKeyDown("space") && gameModeSelection == false && frontPanelRotation.w < 1f)
         {
             backPanelRotation = new Quaternion(1, 1, 1, 1);
             gameModeSelection = true;
-            //rotating = true;
+            print("ENTER PRESSED");
         }
 
-        if (Input.GetKeyDown("enter") && gameModeSelection == true && backPanelRotation.w < 0.001f)
+        if (Input.GetKeyDown("space") && gameModeSelection == true && backPanelRotation.w < 0.001f)
         {
             frontPanelRotation = new Quaternion(0, 0, 0, 0);
             gameModeSelection = false;
-            rotating = true;
         }
 
-        //if (backPanelRotation.w < 0.001f || frontPanelRotation.w < 0.001f){
-        //    rotating = false;
-        //}
+ 
 
 
 
@@ -49,13 +41,16 @@ public class moveCamera : MonoBehaviour
         {
             backPanelRotation = Quaternion.Slerp(myTransform.rotation, Quaternion.LookRotation(backPanel.position - myTransform.position), rotationSpeed * Time.deltaTime);
             myTransform.rotation = backPanelRotation;
+            print("ROTATING GAME SELECTION");
+            print(backPanelRotation.w);
 
         }
 
-        if (gameModeSelection == false && frontPanelRotation.w < 0.999f )
+        if (gameModeSelection == false && frontPanelRotation.w < 0.999999f )
         {
             frontPanelRotation = Quaternion.Slerp(myTransform.rotation, Quaternion.LookRotation(frontPanel.position - myTransform.position), rotationSpeed * Time.deltaTime);
             myTransform.rotation = frontPanelRotation;
+            print("ROTATING TO PART SELECTION");
             print(frontPanelRotation.w);
         }
 
